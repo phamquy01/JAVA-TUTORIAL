9a1009a57c98879f393b64650a8c584c7b601adb
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2024-03-16 11:46:31 +0700

    init project

---
+++ phambinhquy/Hello.java
@@ -0,0 +1,11 @@
+package phambinhquy;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("hello world with java quy");
+		System.out.println("hello world with java quy");
+		System.out.println("hello world with java quy");
+		System.out.println("hello world with java quy");
+		System.out.println("hello world with java quy");
+	}
+}
